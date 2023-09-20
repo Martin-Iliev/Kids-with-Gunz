d1966bbfca67737bca2c3b9fd2ad0c417c478366
Martin Iliev <ilievmarti@gmail.com>
2023-09-20 21:25:54 +0200

    9-20 9:25PM
    finished visual novel style dialogue scripting (with temp images/text)

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 
 public class TextController : MonoBehaviour
 {
@@ -10,17 +11,26 @@ public class TextController : MonoBehaviour
     public MouseLook stopLook;
     public PlayerController stopMove;
     public RawImage textBox;
+    public RawImage Mom;
+    public RawImage KidKitchen;
+    public RawImage KidComingHome;
     public float delay = 0.1f;
     public float displayDuration = 4f;
     private float timer = 0f;
     private bool isDisplaying = false;
-    public List<string> stringList = new List<string>();
+    public List<string> stringListStart = new List<string>();
+    public List<string> stringListKidKitchen = new List<string>();
     [SerializeField] private bool isTriggerable;
     [SerializeField] private bool firstText;
+    private List<string> whatText;
+    [SerializeField] TextMeshProUGUI Name;
     private int currentIndex = 0;
-    private bool isShown = false;
+    private string kidName;
+    private bool talkedKitchen = false;
     public string fullText;
     private string currentText = "";
+    public float interactionDistance = 5f;
+    [SerializeField] Camera playerCam;
 
     [TextArea]
     public string textContent = "Default Text";
@@ -30,7 +40,13 @@ public class TextController : MonoBehaviour
         popupText.gameObject.SetActive(false);
         if (firstText)
         { 
-            StartCoroutine(ShowText(stringList[currentIndex]));
+            StartCoroutine(ShowText(stringListStart[currentIndex]));
+            whatText = stringListStart;
+            kidName = "Sophie";
+            Name.text = kidName;
+            Name.enabled = true;
+            KidComingHome.enabled = true;
+            Mom.enabled = false;
         }
     }
     public IEnumerator ShowText(string text)
@@ -38,6 +54,8 @@ public class TextController : MonoBehaviour
         for (int i = 0; i <= text.Length; i++)
         {
             popupText.gameObject.SetActive(true);
+            textBox.enabled = true;
+            Name.enabled = true;
             isDisplaying = true;
             currentText = text.Substring(0, i);
             popupText.text = currentText;
@@ -48,33 +66,81 @@ public class TextController : MonoBehaviour
     }
     private void Update()
     {
-        if (isDisplaying && popupText.text == stringList[currentIndex])
+        if (isDisplaying && popupText.text == whatText[currentIndex])
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (currentIndex == stringList.Count - 1 && !firstText) 
+                if (Name.text == "Mom")
                 {
-                    enabled = false;
+                    Name.text = kidName; 
+                }
+                else if (Name.text == kidName)
+                {
+                    Name.text = "Mom";
+                }
+                if (firstText)
+                {
+                    if (Name.text == "Mom")
+                    {
+                        Mom.enabled = true;
+                        KidComingHome.enabled = false;
+                    }
+                    if (Name.text == "Sophie")
+                    {
+                        KidComingHome.enabled = true;
+                        Mom.enabled = false;
+                    }
+                }
+                if (currentIndex == whatText.Count - 1 && !firstText) 
+                {
                     popupText.gameObject.SetActive(false);
                     stopLook.enabled = true;
                     stopMove._MovementSpeed = 3f;
                     textBox.enabled = false;
+                    Name.enabled = false;
+                    isDisplaying = false;
                 }
-                if (currentIndex == stringList.Count - 1 && firstText)
+                if (currentIndex == whatText.Count - 1 && firstText)
                 {
-                    enabled = false;
                     popupText.gameObject.SetActive(false);
                     stopLook.enabled = true;
                     stopMove._MovementSpeed = 3f;
+                    Name.enabled = false;
                     stopMove.DishGameStart = true;
                     textBox.enabled = false;
+                    Mom.enabled = false;
+                    KidComingHome.enabled = false;
+                    isDisplaying = false;
                 }
                 popupText.gameObject.SetActive(false);
-                isDisplaying = false;
-                if (currentIndex != stringList.Count - 1) 
+                
+                if (currentIndex != whatText.Count - 1) 
                 {
                     currentIndex++;
-                    StartCoroutine(ShowText(stringList[currentIndex]));
+                    StartCoroutine(ShowText(whatText[currentIndex]));
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && !isDisplaying)
+        {
+            Camera playerCamera = Camera.main;
+            if (playerCamera != null)
+            {
+                RaycastHit hit;
+                bool cast = Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance);
+                if (cast)
+                {
+                    if (hit.transform.CompareTag("kidKitchen") && !talkedKitchen)
+                    {
+                        talkedKitchen = true;
+                        currentIndex = 0;
+                        firstText = false;
+                        whatText = stringListKidKitchen;
+                        kidName = "Billy";
+                        Name.text = kidName;
+                        StartCoroutine(ShowText(whatText[currentIndex]));
+                        Debug.Log("billy text");
+                    }
                 }
             }
         }
@@ -86,7 +152,7 @@ public class TextController : MonoBehaviour
             if (other.CompareTag("toybox"))
             {
                 StartCoroutine(ShowText("Collect all the toys in the toybox!"));
-                isShown = true;
+                //isShown = true;
             }
         }
     }
